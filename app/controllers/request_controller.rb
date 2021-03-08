af3b2bc5921d57f34535277c7cf6af3c8e4b9435
Tatsuya-kanaria <kanaria.git@gmail.com>
2021-03-08 18:56:06 +0900

    finish_test

--- app/controllers/request_controller.rb
+++ app/controllers/request_controller.rb
@@ -1,4 +1,11 @@
 class RequestController < ApplicationController
+  require 'net/http'
+  require 'uri'
+
   def connection
+    url = URI.parse('http://localhhost:3000/api')
+    req = Net::HTTP::Get.new(url.path)
+    res = Net::HTTP.start(url.host, url.port) { |http| http.request(req) }
+    render plain: res.body
   end
 end
